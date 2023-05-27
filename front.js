b4b491999941b76a379a8160e48d2082d8f1477c
swati <iamswatigour@gmail.com>
2023-05-27 02:14:32 -0700

    add contact page

--- front.js
+++ front.js
@@ -22,3 +22,21 @@ function myfunction() {
   function MyClose(){
 confirm("do you want to close this");
   }
+  function initMap() {
+    // Specify your latitude and longitude coordinates
+    var myLatLng = {lat: 37.7749, lng: -122.4194};
+
+    // Create a map centered on the specified coordinates
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatLng,
+      zoom: 12
+      
+    });
+
+    // Add a marker at the specified coordinates
+    var marker = new google.maps.Marker({
+      map: map,
+      position: myLatLng,
+      title: 'Our Location'
+    });
+  }
